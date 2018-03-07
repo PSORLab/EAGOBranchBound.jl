d813f791ba2dbe71fcf18727777a2f3316501f5f
MatthewStuber <33096280+MatthewStuber@users.noreply.github.com>
2018-03-07 09:01:28 -0500

    documenter

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,18 @@
 using Documenter, EAGOBranchBound
 
-makedocs()
+makedocs(modules=[EAGOBranchBound],
+         doctest=false, clean=true,
+         format =:markdown,
+         sitename="EAGO.jl",
+         authors="Matthew Wilhelm",
+         pages = Any[
+         "Home" => "index.md",
+         "Tutorials" => Any[
+           "tutorials/page1.md",
+           "tutorials/page2.md",
+           "tutorials/page3.md"
+         ],
+         "EAGOBranchBound" => Any[
+           "userdocs/page1.md",
+         ]
+         ])
