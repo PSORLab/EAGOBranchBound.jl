bd38f9a458ffd54e735459719888879ca98b6954
Unknown <wilhelmme@gmail.com>
2018-03-19 19:58:29 -0400

    Test Update

--- test/Options_Test.jl
+++ test/Options_Test.jl
@@ -7,21 +7,21 @@ using EAGOBranchBound
 x = BnBSolver()
 
 set_Branch_Scheme!(x,"best")
-@test x.Node_Select == NS_best
-@test x.Branch_Sto == BM_depth_best!
+@test x.Node_Select == EAGOBranchBound.NS_best
+@test x.Branch_Sto == EAGOBranchBound.BM_depth_best!
 
 set_Branch_Scheme!(x,"breadth")
-@test x.Node_Select == NS_depth_breadth
-@test x.Branch_Sto == BM_breadth!
+@test x.Node_Select == EAGOBranchBound.NS_depth_breadth
+@test x.Branch_Sto == EAGOBranchBound.BM_breadth!
 
 set_Branch_Scheme!(x,"depth")
-@test x.Node_Select == NS_depth_breadth
-@test x.Branch_Sto == BM_depth_best!
+@test x.Node_Select == EAGOBranchBound.NS_depth_breadth
+@test x.Branch_Sto == EAGOBranchBound.BM_depth_best!
 
 set_Bisect_Func!(x,"relative midpoint",-1)
-@test x.Bisect_Func == Bisect_Rel
+@test x.Bisect_Func == EAGOBranchBound.Bisect_Rel
 set_Bisect_Func!(x,"absolute midpoint",-1)
-@test x.Bisect_Func == Bisect_Abs
+@test x.Bisect_Func == EAGOBranchBound.Bisect_Abs
 
 set_Verbosity!(x,"Normal")
 @test x.Verbosity == "Normal"
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,10 +7,12 @@ println("Testing 1D Interval Optimization...")
 t = @elapsed include("D1_Interval_Test.jl")
 println("done (took $t seconds).")
 
+
 println("Testing Types...")
 t = @elapsed include("Test_Types.jl")
 println("done (took $t seconds).")
 
+
 println("Testing Option Setting...")
-t = @elapsed include("Options_Types.jl")
+t = @elapsed include("Options_Test.jl")
 println("done (took $t seconds).")
